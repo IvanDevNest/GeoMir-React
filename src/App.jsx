6906357cc01fa6f2c90f6e131c827cdb89d8dd67
Ivan Moscoso <moscosoi2002@gmail.com>
2023-01-19 15:52:55 +0100

    Primera subida al repo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Register } from './assets/Auth/Register'
+import Login from './assets/Auth/Login'
+
+// function App() {
+
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <LoginRegister />
+)
+
+export default App
