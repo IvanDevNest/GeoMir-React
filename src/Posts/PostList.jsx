c97ca4a88181f5f30ff3da7a8ca34af2e5c85366
Arnau Olea <arnauolea@gmail.com>
2023-02-07 15:21:53 +0100

    klk

--- src/Posts/PostList.jsx
+++ src/Posts/PostList.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { UserContext } from '../userContext';
 import { useContext } from 'react';
+import PostsList from './PostsList';
 
 
-export default function PostList() {
+export default function PostList({post}) {
     let { authToken, setAuthToken } = useContext(UserContext);
+    console.log(post);
     return (
-      <div>Post</div>
+      <>
+      <td> {post.body}</td>
+      <td> {post.longitude} </td>
+      <td> {post.latitude} </td>
+      <td> {post.likes_count} </td>
+      <td> {post.author.name} </td>
+      <td> {post.comments_count} </td>
+
+      </>
     )
   }
