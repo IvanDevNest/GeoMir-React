646c77e3ce70f46f77725eb3c71125e9819df7bd
Olea <arnauolea@gmail.com>
2023-02-12 19:56:23 +0100

    postadd

--- src/Posts/PostAdd.jsx
+++ src/Posts/PostAdd.jsx
@@ -1,61 +1,47 @@
 import React, { useEffect } from 'react'
-import { useState } from 'react';
+import { useState } from "react";
 import { useContext } from "react";
 import { UserContext } from "../userContext";
-import { handleChange } from 'react';
+import { useNavigate } from "react-router-dom";
 
-
-export default function PostAdd({ setCanvi }) {
-
-
-  let { authToken, setAuthToken } = useContext(UserContext);
-  let [formulari, setFormulari] = useState({})
+const PostCreate = () => {
+  let navigate = useNavigate();
+  let [formulari, setFormulari] = useState({});
   let [error, setError] = useState("");
-  let { body, upload, longitude, latitude, visibility} = formulari;
-  const formData = new FormData();
-  formData.append("body", body);
-  formData.append("upload", upload);
-  formData.append("longitude", longitude);
-  formData.append("latitude", latitude);
-  formData.append("visibility", visibility);
- 
-  useEffect(()=>{
-    navigator.geolocation.getCurrentPosition((pos) => {
+  let { authToken, setAuthToken } = useContext(UserContext);
 
-      setFormulari({
-  
-  
-        ...formulari,
-        latitude: pos.coords.latitude,
-        longitude: pos.coords.longitude
-  
-      })
-  
-      console.log("Latitude is :", pos.coords.latitude);
-      console.log("Longitude is :", pos.coords.longitude);
-    });
-    
-  
-  },[]);
+
 
   const handleChange = (e) => {
-    if (e.target.type && e.target.type==="file")
-  {
+    if (e.target.type && e.target.type === "file") {
+      setFormulari({
+        ...formulari,
+        [e.target.name]: e.target.files[0]
+      })
+    } else {
+      setFormulari({
+        ...formulari,
+        [e.target.name]: e.target.value
+      })
+    }
+  };
+  const handleReset = (e) => {
+    e.preventDefault()
     setFormulari({
       ...formulari,
-      [e.target.name] : e.target.files[0] 
+      body: "",
+      upload: ""
     })
-  } else {
-    setFormulari({
-      ...formulari,
-      [e.target.name] : e.target.value
-  })}
-  }
-
-  const SendPost = async (e) => {
+  };
+  const handleCreate = async (e) => {
     e.preventDefault();
-
-    // Enviam dades a l'aPI i recollim resultat
+    let { body, upload, latitude, longitude, visibility = 1} = formulari;
+    const formData = new FormData();
+    formData.append("body", body);
+    formData.append("upload", upload);
+    formData.append("latitude", latitude);
+    formData.append("longitude", longitude);
+    formData.append("visibility", visibility);
     try {
       const data = await fetch("https://backend.insjoaquimmir.cat/api/posts", {
         headers: {
@@ -65,62 +51,77 @@ export default function PostAdd({ setCanvi }) {
         method: "POST",
         body: formData
       });
-
-
       const resposta = await data.json();
-      console.log(resposta)
-      if (resposta.success === true) setAuthToken(resposta.authToken);
-      else alert("La resposta no ha triomfat");
-
-
+      if (resposta.success === true) {
+        console.log("post creado")
+        navigate("/posts/" + resposta.data.id)
+      } else {
+        console.log(resposta.message)
+        setError(resposta.message);
+      }
     } catch {
       console.log("Error");
       alert("catch");
-    }
-  };
+    };
+  }
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      setFormulari({
+        ...formulari,
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude
 
-
-
-
-
+      })
+      console.log("Latitude is :", pos.coords.latitude);
+      console.log("Longitude is :", pos.coords.longitude);
+    });
+  },[])
   return (
-    <>
+    <div>
+      <div className="card ">
+        <div className="card-header ">
 
-      <div class="login-form">
-        <h1>Afegir Post +</h1>
+          <h1 className="text-center h2 fw-bold">Crear Post</h1>
 
-        <i class="fa fa-user"></i>
-        <form>
-          <label for="name">Body</label><br></br>
-          <input class="form-control" type="text" name="body" value={formulari.body} onChange={handleChange} ></input><br></br>
-          <label for="longitud">Longitud</label><br></br>
-          <input class="form-control" type="text" name="longitud" value={formulari.longitude} onChange={handleChange}></input><br></br>
-          <label for="latitud">Latitud</label><br></br>
-          <input class="form-control" type="text" name="latitud" value={formulari.latitude} onChange={handleChange} ></input><br></br>
-          <label for="upload">Archivo</label><br></br>
-          <input class="form-control" type="file" name="upload" value={formulari.file} onChange={handleChange} ></input><br></br>
+        </div >
+        <form method="post" className="separar " action="{{ route('posts.store') }}" enctype="multipart/form-data">
+          <div className="form-group">
+            <label for="body">Body</label>
+            <input type="text" value={formulari.body} onChange={handleChange} name="body" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label for="upload">File</label>
+            <input type="file" value={formulari.file} onChange={handleChange} name="upload" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label for="latitude">Latitude</label>
+            <input value={formulari.latitude} onChange={handleChange} name="latitude" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label for="longitude">Longitude</label>
+            <input value={formulari.longitude} onChange={handleChange} name="longitude" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label for="visibility">Visibility</label>
 
+            <select name="visibility" value={formulari.visibility} onChange={handleChange} className="form-control"  >
+              <option value="1" selected>public</option>
+              <option value="2">contacts</option>
+              <option value="3">private</option>
+            </select>
 
-          <label for="visibility">Visibilidad</label><br></br>
-          <input class="form-control" type="text" name="visibility" value={formulari.visibility} onChange={handleChange}></input><br></br>
-        </form>
-
-        <button
-          onClick={(e) => {
-            SendPost(e);
-          }}
-        >
-          Add post      </button>
-        {error ? <div>{error}</div> : <></>}
-
-        <br></br><button
-        >
-          reset
-        </button>
-
+          </div>
+          <button className="btn btn-primary" onClick={(e) => {
+            handleCreate(e);
+          }}>Create</button>
+          <button className="btn btn-secondary" onClick={(e) => {
+            handleReset(e)
+          }}>Reset</button>
+          {error ? (<div>{error}</div>) : (<></>)}        </form>
       </div>
-    </>
-  );
+    </div>
+  )
 }
 
+export default PostCreate
