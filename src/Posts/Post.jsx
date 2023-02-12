37ab89104bb10a086f780b6d0028316f912f071e
Olea <arnauolea@gmail.com>
2023-02-12 19:44:51 +0100

    post a tope

--- src/Posts/Post.jsx
+++ src/Posts/Post.jsx
@@ -1,11 +1,84 @@
 import React from 'react'
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import { useContext } from "react";
+import { UserContext } from "../userContext";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import PostsGrid from './PostsGrid';
 
 
+const Post = () => {
+  let [isLoading, setLoading] = useState(true)
+  let { authToken, setAuthToken } = useContext(UserContext);
+  let [post, setPost] = useState([]);
 
-export default function Post() {
   const { id } = useParams();
+  const getPost = async () => {
+    try {
+      const data = await fetch("https://backend.insjoaquimmir.cat/api/posts/" + id, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          'Authorization': 'Bearer ' + authToken,
+        },
+        method: "POST",
+      });
+      const resposta = await data.json();
+      console.log(isLoading)
+
+      if (resposta.success === true) {
+        console.log(resposta)
+        setPost(resposta.data)
+        setLoading(false)
+        console.log(isLoading)
+
+      }
+      else setError(resposta.message);
+    } catch {
+      console.log("Error");
+      alert("Catchch");
+    };
+
+  }
+  useEffect(() => {
+    getPost();
+
+  }, []);
+
+
   return (
-    <div>Post</div>
+    <>
+
+      {isLoading ? "cargando..." : <>
+        <table>
+          <tr>
+            <td>ID</td>
+            <td>{post.id}</td>
+          </tr>
+          <tr>
+            <td>Nombre</td>
+            <td>{post.name}</td>
+
+
+          </tr>
+
+          <tr>
+            <td>Autor</td>
+            <td>{post.author.name}</td>
+
+          </tr>
+          <tr>
+            <td>Descripcio</td>
+            <td>{post.description}</td>
+
+          </tr>
+        </table>
+
+      </>
+      }
+
+    </>
   )
 }
+
+export default Post
